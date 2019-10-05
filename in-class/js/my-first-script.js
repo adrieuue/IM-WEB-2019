939c18d1ad94fd3cbc952e590abb7c2531981e21
adrieuue <55204870+adrieuue@users.noreply.github.com>
2019-10-05 01:57:53 -0400

    in class js walkthrough files

---
+++ in-class/js/my-first-script.js
@@ -0,0 +1,11 @@
+// alert ("my first event");
+
+
+// console.log("this is my secret message to me");
+
+
+
+$("button").click(function(){
+	// alert("my first REAL function")
+	$("body").css("background", "blue")
+})
